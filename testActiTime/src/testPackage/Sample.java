3214a2c804ef08855c277fd015ddf34939092a08
prachipati <100658554+prachipati@users.noreply.github.com>
2022-03-01 09:22:33 +0530

    Update Sample.java

--- testActiTime/src/testPackage/Sample.java
+++ testActiTime/src/testPackage/Sample.java
@@ -1,9 +1,9 @@
-package testPackage;
+gpackage testPackage;
 
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("Good Morning");
+		System.out.println("Good Evening");
 
 	}
 
